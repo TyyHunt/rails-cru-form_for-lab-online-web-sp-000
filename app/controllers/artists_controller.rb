53fc2a0de26b2b377dcef75bb1a965c303f55a7c
Tyler Hunt <tyhunt04@gmail.com>
2019-04-26 15:43:23 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -18,11 +18,14 @@ class ArtistsController < ApplicationController
     redirect_to artist_path(@artist)
   end
 
-  def update
-  end
-
   def edit
+    @artist = Artist.find(params[:id])
   end
 
+  def update
+    @artist = Artist.find(params[:id])
+    @artist = Artist.update(params.require(:artist).permit(:name, :bio))
+    redirect_to artist_path(@artist)    
+  end
 
 end
