bf5215e8ace3b04698bba417cf7d2a5d2aa11ac2
Tyler Hunt <tyhunt04@gmail.com>
2019-04-26 15:39:55 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,14 @@
 class ArtistsController < ApplicationController
+  
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
-  def new
+  def new 
   end
 
   def create
